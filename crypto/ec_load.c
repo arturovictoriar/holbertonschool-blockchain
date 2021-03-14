3bbfec8e61caf570f99bb294ea1a56e85c3c7654
arturovictoriar <1231@holbertonschool.com>
2021-03-14 04:33:19 +0000

    load an EC key pair from the disk

---
+++ crypto/ec_load.c
@@ -0,0 +1,49 @@
+#include "hblk_crypto.h"
+
+/**
+* ec_load - loads an EC key pair from the disk.
+* @folder: path to the folder in which to save the keys
+* Return: return a pointer to the created EC key pair
+* upon success, or NULL upon failure
+*/
+EC_KEY *ec_load(char const *folder)
+{
+	FILE *fp;
+	char path[256] = {0};
+	EC_KEY *key = NULL;
+
+	if (!folder)
+		return (0);
+
+	/*Create the complete path to read the private key*/
+	sprintf(path, "%s/" PRI_FILENAME, folder);
+	/*Open a file descriptor to read the private key*/
+	fp = fopen(path, "r");
+	if (!fp)
+	{
+		EC_KEY_free(key);
+		return (0);
+	}
+	/*read the private key in the given file and store it in key*/
+	if (!PEM_read_ECPrivateKey(fp, &key, NULL, NULL))
+	{
+		EC_KEY_free(key);
+		fclose(fp);
+		return (0);
+	}
+	fclose(fp);
+	/*Create the complete path to read the public key*/
+	sprintf(path, "%s/" PUB_FILENAME, folder);
+	/*Open a file descriptor to read the public key*/
+	fp = fopen(path, "r");
+	if (!fp)
+		return (0);
+	/*Read the public key in the given file and store it in key*/
+	if (!PEM_read_EC_PUBKEY(fp, &key, NULL, NULL))
+	{
+		fclose(fp);
+		return (0);
+	}
+	fclose(fp);
+	return (key);
+}
--- crypto/hblk_crypto.h
+++ crypto/hblk_crypto.h
@@ -45,5 +45,6 @@ EC_KEY *ec_create(void);
 uint8_t *ec_to_pub(EC_KEY const *key, uint8_t pub[EC_PUB_LEN]);
 EC_KEY *ec_from_pub(uint8_t const pub[EC_PUB_LEN]);
 int ec_save(EC_KEY *key, char const *folder);
+EC_KEY *ec_load(char const *folder);
 
 #endif
