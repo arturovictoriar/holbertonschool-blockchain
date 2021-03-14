8be56c4812380678d57c8912bb93c1b9b98dda8a
arturovictoriar <1231@holbertonschool.com>
2021-03-14 02:48:39 +0000

    Delete group key

--- crypto/ec_from_pub.c
+++ crypto/ec_from_pub.c
@@ -7,32 +7,30 @@
  */
 EC_KEY *ec_from_pub(uint8_t const pub[EC_PUB_LEN])
 {
-	EC_KEY *key = NULL;
-	const EC_GROUP *group = NULL;
-	EC_POINT *point = NULL;
+	EC_KEY *key;
+	EC_POINT *point;
 
+	if (!pub)
+		return (NULL);
 	/*Create a key from the curve name*/
 	key = EC_KEY_new_by_curve_name(EC_CURVE);
-	/*Get the group of the key*/
-	group = EC_KEY_get0_group(key);
+	if (!key)
+		return (NULL);
 	/*Create a point*/
-	point = EC_POINT_new(group);
-	if (point == NULL)
+	point = EC_POINT_new(EC_KEY_get0_group(key));
+	if (!point)
 	{
 		EC_KEY_free(key);
 		return (NULL);
 	}
-
 	/*creates an EC_KEY structure given a public key*/
-	if (!EC_POINT_oct2point(group, point, pub, EC_PUB_LEN, NULL) ||
-			!EC_KEY_set_public_key(key, (const EC_POINT *) point))
+	if (!EC_POINT_oct2point(EC_KEY_get0_group(key), point, pub, EC_PUB_LEN, NULL)
+		|| !EC_KEY_set_public_key(key, point))
 	{
 		EC_KEY_free(key);
 		EC_POINT_free(point);
 		return (NULL);
 	}
-
-
 	EC_POINT_free(point);
 	return (key);
 }
