1849fd4a7390e6d18c7e32f51334220de663912a
arturovictoriar <1231@holbertonschool.com>
2021-03-14 02:42:49 +0000

    Add the general documentation. Handle error in openssl function

--- crypto/ec_from_pub.c
+++ crypto/ec_from_pub.c
@@ -1,25 +1,38 @@
 #include "hblk_crypto.h"
 
+/**
+ * ec_from_pub - creates an EC_KEY structure given a public key
+ * @pub: contains the public key to be converted
+ * Return: EC_KEY structure
+ */
 EC_KEY *ec_from_pub(uint8_t const pub[EC_PUB_LEN])
 {
 	EC_KEY *key = NULL;
-	BN_CTX *ctx = NULL;
 	const EC_GROUP *group = NULL;
 	EC_POINT *point = NULL;
 
+	/*Create a key from the curve name*/
 	key = EC_KEY_new_by_curve_name(EC_CURVE);
-	/*Initialize a temporaly Big Number*/
-	ctx = BN_CTX_new();
-	/*Get the curve of the key*/
+	/*Get the group of the key*/
 	group = EC_KEY_get0_group(key);
+	/*Create a point*/
 	point = EC_POINT_new(group);
+	if (point == NULL)
+	{
+		EC_KEY_free(key);
+		return (NULL);
+	}
 
-	EC_POINT_oct2point(group, point,
-			(const unsigned char *) pub, EC_PUB_LEN, ctx);
-	EC_KEY_set_public_key(key, (const EC_POINT *) point);
+	/*creates an EC_KEY structure given a public key*/
+	if (!EC_POINT_oct2point(group, point, pub, EC_PUB_LEN, NULL) ||
+			!EC_KEY_set_public_key(key, (const EC_POINT *) point))
+	{
+		EC_KEY_free(key);
+		EC_POINT_free(point);
+		return (NULL);
+	}
 
-	BN_CTX_free(ctx);
+
 	EC_POINT_free(point);
-
 	return (key);
 }
