f0fd83ca6232b296bd5a4ea3757bca3f126086d6
arturovictoriar <1231@holbertonschool.com>
2021-03-25 03:36:07 +0000

    Fix betty style

--- blockchain/v0.1/block_is_valid.c
+++ blockchain/v0.1/block_is_valid.c
@@ -11,12 +11,16 @@ int block_is_valid(block_t const *block, block_t const *prev_block)
 	uint8_t hash_buffer[SHA256_DIGEST_LENGTH] = {0};
 	block_t GENESIS = GENESIS_BLOCK;
 
-	/*block should not be NULL and prev_block should be
-	  NULL ONLY if block‘s index is 0*/
+	/*
+	 *block should not be NULL and prev_block should be
+	 *NULL ONLY if block‘s index is 0
+	 */
 	if (!block || (!prev_block && block->info.index != 0))
 		return (1);
-	/*If the Block index is 0, the Block should
-	  match the Genesis Block described in the first task*/
+	/*
+	 *If the Block index is 0, the Block should
+	 *match the Genesis Block described in the first task
+	 */
 	if (block->info.index == 0)
 		return (memcmp(block, &GENESIS, sizeof(GENESIS)));
 	/*The Blocks index must be the previous Blocks index, plus 1*/
@@ -26,8 +30,10 @@ int block_is_valid(block_t const *block, block_t const *prev_block)
 	if (!block_hash(prev_block, hash_buffer) ||
 			memcmp(hash_buffer, prev_block->hash, SHA256_DIGEST_LENGTH))
 		return (1);
-	/*The computed hash of the previous block must match the reference one
-	  stored in block*/
+	/*
+	 *The computed hash of the previous block must match the reference one
+	 *stored in block
+	 */
 	if (memcmp(prev_block->hash, block->info.prev_hash, SHA256_DIGEST_LENGTH))
 		return (1);
 	/*The computed hash of the Block must match the one it stores*/
