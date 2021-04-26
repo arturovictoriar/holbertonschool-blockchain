61e87483a0fe25b554120e9621d2e7936bba702b
arturovictoriar <1231@holbertonschool.com>
2021-04-26 03:03:36 +0000

    Update block create to initialize the adds transaction list to an empty linked listBlock

--- blockchain/v0.3/block_create.c
+++ blockchain/v0.3/block_create.c
@@ -10,16 +10,23 @@
 block_t *block_create(block_t const *prev, int8_t const *data,
 	uint32_t data_len)
 {
-	block_t *block = calloc(1, sizeof(*block));
-	llist_t *transactions = llist_create(MT_SUPPORT_FALSE);
+	block_t *block = NULL;
+	llist_t *transactions = NULL;
 
-	if (!block || !transactions)
-		return (free(block), llist_destroy(transactions, 0, NULL), NULL);
+	block = calloc(1, sizeof(block));
+	transactions = llist_create(MT_SUPPORT_FALSE);
+	if (block == NULL || transactions == NULL)
+	{
+		free(block);
+		llist_destroy(transactions, 0, NULL);
+		return (NULL);
+	}
 	memcpy(&(block->data.buffer), data, MIN(data_len, BLOCKCHAIN_DATA_MAX));
 	block->data.len = MIN(data_len, BLOCKCHAIN_DATA_MAX);
 	block->info.index = prev->info.index + 1;
 	block->info.timestamp = time(NULL);
 	block->transactions = transactions;
 	memcpy(&(block->info.prev_hash), prev->hash, SHA256_DIGEST_LENGTH);
+
 	return (block);
 }
