db04caeaebf4b2fb021e3c3c64db5663fc2e9f8c
arturovictoriar <1231@holbertonschool.com>
2021-04-26 03:04:59 +0000

    Update block destroy to destroy the adds transaction list.Block

--- blockchain/v0.3/block_destroy.c
+++ blockchain/v0.3/block_destroy.c
@@ -6,8 +6,11 @@
  */
 void block_destroy(block_t *block)
 {
-	if (!block)
+	if (block == NULL)
+	{
 		return;
+	}
+
 	llist_destroy(block->transactions, 1, (node_dtor_t)transaction_destroy);
 	free(block);
 }
